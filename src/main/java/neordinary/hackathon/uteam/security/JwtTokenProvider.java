db99acf72cd11c11b49323a1ed91c1c47cafe54a
Wo-ogie <siwall0105@gmail.com>
2023-06-10 16:30:12 +0900

    refactor: access token 만료기한 수정. 12시간 => 하루

--- src/main/java/neordinary/hackathon/uteam/security/JwtTokenProvider.java
+++ src/main/java/neordinary/hackathon/uteam/security/JwtTokenProvider.java
@@ -35,7 +35,7 @@ public class JwtTokenProvider {
     private static final long MINUTE = 1000 * 60L;
     private static final long HOUR = 60 * MINUTE;
     private static final long DAY = 24 * HOUR;
-    private static final long ACCESS_TOKEN_EXPIRED_DURATION = 12 * HOUR; // Access token 만료시간 : 12시간
+    private static final long ACCESS_TOKEN_EXPIRED_DURATION = DAY; // Access token 만료시간 : 하루
     public static final long REFRESH_TOKEN_EXPIRED_DURATION = 30 * DAY; // Refresh token 만료시간 : 한 달
 
     private static final String TOKEN_TYPE_BEARER = "Bearer ";
