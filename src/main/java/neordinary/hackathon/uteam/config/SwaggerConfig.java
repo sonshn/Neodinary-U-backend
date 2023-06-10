9ce4c55edd68424c5af6e24ed42ffe5edb74eb3f
Wo-ogie <siwall0105@gmail.com>
2023-06-10 14:47:22 +0900

    chore: swagger 의존성 추가 및 설정

---
+++ src/main/java/neordinary/hackathon/uteam/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package neordinary.hackathon.uteam.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI eateryApi() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Eatery API Docs")
+                                .description("너디너리 해커톤 4th 프로젝트")
+                                .version("0.0.1")
+                )
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("Github organization of team u")
+                                .url("https://github.com/Neodinary-U")
+                );
+    }
+}
